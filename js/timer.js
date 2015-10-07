fc6f1c85c310da27430ce9ce39d09c14e8848ae9
chris jones <cjones@cbnco.com>
2015-10-07 16:47:53 -0400

    update

--- js/timer.js
+++ js/timer.js
@@ -33,8 +33,8 @@ function startUIUpdater() {
                 item_count_map[ item ] += adjust;
             }
             getElement(item).value = numberFormat(item_count_map[item]);
-            console.log("adding", item_count_map[item] * Math.pow(BASE, 1-items_arr.length), 'value from', item);
-            total_value += item_count_map[item] * Math.pow(BASE, 1-items_arr.length);
+            console.log("adding", item_count_map[item] * Math.pow(BASE, i+1-items_arr.length), 'value from', item);
+            total_value += item_count_map[item] * Math.pow(BASE, i+1-items_arr.length);
         }
        
         getElement("total_value").value = numberFormat(total_value);

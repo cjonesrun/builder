216702b129fc1d75222643285f23db2c100d48b0
chris jones <cjonesrun@gmail.com>
2017-01-03 08:43:37 -0500

    more v5 dev

--- v5/js/const.js
+++ v5/js/const.js
@@ -37,3 +37,18 @@ var machineParts = [
 	'instrument', 'harness', 'kit', 'accessory', 'whatchamacalit', 'paraphernalia',
 	'thingamajig','apparatus', 'appliance', 'furnishing', 'rig', 'rube goldberg'
 ];
+
+// id, innerHTML, title
+var optionsButtons = [
+		["message_button","show messages","show message box"],
+		["pause_button","pause","pause/resume game ticker"],
+		["save_button","save","save game state"],
+		["clear_button","clear messages","clear messages"],
+		["export_button","export","export serialized game state to messages"],
+		["load_button","load","load serialized game state from messages"],
+		["export_json_button","JSON","show game state JSON"],
+		["stats","stats","show game stats"],
+		["config","config","config rbg parameters"],
+		["reset_button","reset","reset game"],
+		["hard_reset","hard","blow away local storage"]
+	];
--- v5/js/init.js
+++ v5/js/init.js
@@ -13,24 +13,11 @@ function buildUI() {
 function buildOptionsDiv() {
 	var className = "option_button";
 	
-	// id, innerHTML, title
-	var options = [
-		["message_button","show messages","show message box"],
-		["pause_button","pause","pause/resume game ticker"],
-		["save_button","save","save game state"],
-		["clear_button","clear messages","clear messages"],
-		["export_button","export","export serialized game state to messages"],
-		["load_button","load","load serialized game state from messages"],
-		["export_json_button","JSON","show game state JSON"],
-		["stats","stats","show game stats"],
-		["config","config","config rbg parameters"],
-		["reset_button","reset","reset game"],
-		["hard_reset","hard","blow away local storage"]
-	];
+	
 
 	var options_div = document.getElementById("options_div");
-	for (var i=0; i<options.length; i++) {
-		c=options[i];
+	for (var i=0; i<optionsButtons.length; i++) {
+		c=optionsButtons[i];
 
 		var d = div(c[0], className, c[1], c[2]);
 		options_div.appendChild(d);
